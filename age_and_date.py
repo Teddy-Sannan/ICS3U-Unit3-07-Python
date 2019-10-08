6738f9cbf98e5787c6b400784ebe3d13dd896007
Ubuntu <ubuntu@ip-172-31-16-204.ec2.internal>
2019-10-08 17:21:45 +0000

    age and dateing

---
+++ age_and_date.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# Created by: Teddy Sannan
+# Created on: October 2019
+# This lets the user guess a number
+
+
+def main():
+    while True:
+        try:
+            age = int(input("Please enter your age: "))
+
+            if age >= 25 and age <= 40:
+                print()
+                print("You can date my grandchild")
+
+            elif age < 25 or age > 40:
+                print()
+                print("You cannot date my grandchild")
+
+        except ValueError:
+            print()
+            print("Invalid Input")
+            print()
+            continue
+
+        else:
+            break
+
+
+if __name__ == "__main__":
+    main()
